4e8b29eedd514d10b209c8de3d5e8024c88e3398
ridho-akbar <ridho-akbar@cookpad.com>
2024-12-08 21:41:35 +0000

    Update markov_chain model

--- models/markov_chain.py
+++ models/markov_chain.py
@@ -6,7 +6,7 @@ def random_walks(Q, max_steps):
     current_state = np.random.choice(n_states) # start at random node
     states = [] # list to keep track the visited states in each iteration
     freq = [] # list to keep track how many times each states has been visited. Initial value is 1 for state-A
-    for step in range(max_steps):
+    for step in range(max_steps + 1):
         states.append(current_state) # keep track of the current state
         # count the frequency of visit of each state
         f_ = count_states_frequency(states)
@@ -26,19 +26,20 @@ def random_particle_migrations(Q, particles_dist, max_steps):
     s1 = np.zeros(particles_dist[0]).astype(int)
     s2 = np.ones(particles_dist[1]).astype(int)
     current_states = np.concatenate([s1,s2])
-    particle_counts = []
-    for step in range(max_steps):
+    particle_counts, states = [], []
+    for step in range(max_steps + 1):
         c = count_states_particles(current_states, n_states)
         particle_counts.append(c)
+        states.append(current_states)
         # move particles randomly according to Q
         current_states = np.array([np.random.choice(n_states, p=Q[s]) for s in current_states])
 
-    return np.array(particle_counts), current_states
+    return np.array(particle_counts), np.array(states)
 
 
 def count_states_particles(states, n_states):
-    particles_count = np.zeros(n_states)
-    count_ = count_frequency(states)
+    particles_count = np.zeros(n_states).astype(int)
+    count_ = count_states_frequency(states)
     for k,v in count_.items():
         particles_count[k] = v  
     return particles_count
