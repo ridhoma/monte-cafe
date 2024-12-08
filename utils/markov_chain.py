82104586f02915e4ec74d17229869160c5732e96
ridho-akbar <ridho-akbar@cookpad.com>
2024-12-08 00:02:21 +0000

    Create utils/markov_chain.py

---
+++ utils/markov_chain.py
@@ -0,0 +1,65 @@
+import numpy as np
+
+
+def random_walks(Q, max_steps):
+    n_states = Q.shape[0]
+    current_state = np.random.choice(n_states) # start at random node
+    states = [] # list to keep track the visited states in each iteration
+    freq = [] # list to keep track how many times each states has been visited. Initial value is 1 for state-A
+    for step in range(max_steps):
+        states.append(current_state) # keep track of the current state
+        # count the frequency of visit of each state
+        f_ = count_states_frequency(states)
+        f = np.zeros(n_states)
+        for k,v in f_.items():
+            f[k] = v  
+        freq.append(f)
+        #  random walk to next node according to Q, and update current_state
+        current_state = np.random.choice(n_states, p=Q[current_state])
+    
+    # return as numpy array
+    return np.array(freq), np.array(states)
+
+
+def random_particle_migrations(Q, particles_dist, max_steps):
+    n_states = Q.shape[0]
+    s1 = np.zeros(particles_dist[0]).astype(int)
+    s2 = np.ones(particles_dist[1]).astype(int)
+    current_states = np.concatenate([s1,s2])
+    particle_counts = []
+    for step in range(max_steps):
+        c = count_states_particles(current_states, n_states)
+        particle_counts.append(c)
+        # move particles randomly according to Q
+        current_states = np.array([np.random.choice(n_states, p=Q[s]) for s in current_states])
+
+    return np.array(particle_counts), current_states
+
+
+def count_states_particles(states, n_states):
+    particles_count = np.zeros(n_states)
+    count_ = count_frequency(states)
+    for k,v in count_.items():
+        particles_count[k] = v  
+    return particles_count
+
+
+def count_states_frequency(states):
+    states_count = {}
+    for s in states:
+        if s in states_count:
+            states_count[s] += 1
+        else:
+            states_count[s] = 1
+    return states_count
+
+
+def calculate_stationary_distribution(Q):
+    # system of linear equations A.x = b, solve for x
+    A = Q.T - np.eye(Q.shape[0])
+    A = np.vstack((A, np.ones(Q.shape[0])))
+    b = np.zeros(Q.shape[0])
+    b = np.append(b, 1)
+    pi = np.linalg.lstsq(A, b, rcond=None)[0]
+    return pi
+
--- utils/utils.py
+++ utils/utils.py
@@ -1,14 +1,5 @@
 import numpy as np
 
-def count_frequency(states):
-    states_count = {}
-    for s in states:
-        if s in states_count:
-            states_count[s] += 1
-        else:
-            states_count[s] = 1
-    return states_count
-
 def split_list_to_batch(lst, batch_size):
     if type(lst) != list:
         lst = list(lst)
